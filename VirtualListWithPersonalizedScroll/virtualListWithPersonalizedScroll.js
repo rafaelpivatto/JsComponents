010dcd3361893fc7e4a3d3255f38a2cc739173c0
rpivatto <rpivatto@ciandt.com>
2018-11-21 18:01:44 -0200

    Improvements

--- VirtualListWithPersonalizedScroll/virtualListWithPersonalizedScroll.js
+++ VirtualListWithPersonalizedScroll/virtualListWithPersonalizedScroll.js
@@ -4,26 +4,21 @@ https://sergimansilla.com/blog/virtual-scrolling/
 */
 
 class VirtualList {
-  createScroller(totalHeight) {
+  createScroller() {
     this.scroller = this.container.querySelector('[data-virtual-list-scroller]');
-    this.scroller.classList.add('scroller');
     this.scroller.style.position = 'absolute';
-    this.scroller.style.width = '1px';
-    this.scroller.style.height = `${totalHeight}px`;
-    this.scroller.innerHTML = '';
+    this.scroller.style.height = `${this.totalHeight}px`;
   }
 
   renderChunk(fromPos, howMany) {
-    let item;
     const fragment = document.createDocumentFragment();
+    let item,
+      finalItem = fromPos + howMany;
 
-    let finalItem = fromPos + howMany;
     if (finalItem > this.totalRows) finalItem = this.totalRows;
-
+    
     for (let i = fromPos; i < finalItem; i += 1) {
       item = this.items[i];
-      item.classList.add('vrow'); // avaliar se necessário
-      item.style.top = `${i * this.itemHeight}px`; //poderia remover em caso de não ser absolute?
       fragment.appendChild(item);
     }
 
@@ -32,37 +27,70 @@ class VirtualList {
   }
 
   onScroll(e) {
-    let lastRepaintY;
-    const maxBuffer = this.screenItemsLen * this.itemHeight;
-    const { scrollTop } = e.target;
-    let first = parseInt(scrollTop / this.itemHeight, 10) - this.screenItemsLen;
+    /*
+    const maxBuffer = this.screenItemsLen * this.itemHeight,
+      { scrollTop } = e.target;
+    let lastRepaintY,
+      first = parseInt(scrollTop / this.itemHeight, 10) - this.screenItemsLen
+    
     first = first < 0 ? 0 : first;
+    
     if (!lastRepaintY || Math.abs(scrollTop - lastRepaintY) > maxBuffer) {
       this.renderChunk(first, this.cachedItemsLen);
       lastRepaintY = scrollTop;
     }
+    */
+    const { scrollTop } = e.target;
+    let itemsAboveOfView = 0,
+      itemsToView = 0,
+      pixelsAboveofView = 0,
+      pixelFitsInsideContainer = 0,
+      indexFirstItemToPrint,
+      indexLastItemToPrint;
+
+    for (let i = 0; i <= this.itemsHeight.length ; i++) {
+      if (pixelsAboveofView < scrollTop) {
+        itemsAboveOfView++;
+      } else {
+        itemsToView++;
+        pixelFitsInsideContainer += this.itemsHeight[i];
+        if (pixelFitsInsideContainer > this.container.clientHeight) {
+          break;
+        }
+      }
+      pixelsAboveofView += this.itemsHeight[i];
+    }
+
+    indexFirstItemToPrint = itemsAboveOfView -1 - 5; //5 value will be set 
+    indexFirstItemToPrint = indexFirstItemToPrint < 0 ? 0 : indexFirstItemToPrint;
+    indexLastItemToPrint = indexFirstItemToPrint + itemsToView -1 + 10; //10 value will be set
+
+    this.renderChunk(indexFirstItemToPrint, indexLastItemToPrint);
+
     e.preventDefault();
   }
 
   constructor(container) {
     this.container = container;
 
-    //const list = [];
-    //this.items = this.container.querySelectorAll('[data-virtual-list-element]');
-    const list = this.container.querySelectorAll('[data-virtual-list-element]');
+    const list = this.container.querySelectorAll('li'),
+      itemsHeight = [];
+    let totalHeight = 0,
+      top = 0;
 
-    /*
-    let totalHeight = 0;
     list.forEach((item) => {
       totalHeight += item.clientHeight;
+      item.style.top = `${top}px`;
+      top += parseInt(item.clientHeight);
+      itemsHeight.push( item.clientHeight);
     });
     this.totalHeight = totalHeight;
-    */
     this.items = list;
+    this.itemsHeight = itemsHeight;
 
     this.totalRows = this.items && this.items.length;
-    this.itemHeight = this.container.querySelector('[data-virtual-list-element]').clientHeight;
-    this.createScroller(this.itemHeight * this.totalRows); //totalHeight
+    this.itemHeight = this.container.querySelector('li').clientHeight;
+    this.createScroller();
     
     this.screenItemsLen = Math.ceil(this.container.clientHeight / this.itemHeight) + 1 || 20; //qtos cabem na tela
     this.cachedItemsLen = this.screenItemsLen * 3; //qtos itens a serem carregados
